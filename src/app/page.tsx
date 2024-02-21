ff9a617357adae95840efab322de240a9df982eb
Kevin <keviinnalvarez2015@gmail.com>
2024-02-21 11:31:59 -0400

    config fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import { titleFont } from '@/config/fonts';
 
 export default function Home() {
   return (
     <main>
       This is TesloShop
+      <div className={titleFont.className}>Ecommerce</div>
     </main>
   );
 }
