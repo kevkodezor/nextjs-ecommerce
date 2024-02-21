a76dd00fa423ecb55dc9a6241d649fbabba75c8d
Kevin <keviinnalvarez2015@gmail.com>
2024-02-21 13:02:39 -0400

    Small corrections

---
+++ src/app/auth/login copy/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from '@/config/fonts';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <main>
+      This is TesloShop
+      <div className={titleFont.className}>Shop</div>
+    </main>
+  );
+}
---
+++ src/app/shop/page copy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { titleFont } from '@/config/fonts';
+
+export default function Home() {
+  return (
+    <main>
+      This is TesloShop
+      <div className={titleFont.className}>Shop</div>
+    </main>
+  );
+}
