61b0497047e6ce5041bba88596e3f552e87827f0
Kevin <keviinnalvarez2015@gmail.com>
2024-02-23 16:18:36 -0400

    page by product slug

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,9 +1,36 @@
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed";
+import { notFound } from "next/navigation";
 
+interface Props {
+    params: { slug: string; }
+}
 
-export default function () {
+export default function ({ params }: Props) {
+
+    const { slug } = params;
+    const product = initialData.products.find(product => product.slug === slug);
+
+    if (!product) notFound;
+
     return (
-        <div>
-            Product Slug
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-3 lg:px-5'>
+
+            <div className='col-span-1 md:col-span-2'>
+                <h1>Producto </h1>
+            </div>
+
+            <div className='col-span-1 gap-3 grid'>
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product?.title}</h1>
+                <p className='text-lg'>$ {product?.price}</p>
+
+                <button className='btn-primary'>
+                    Agregar al carrito
+                </button>
+                <h3 className='font-bold text-sm'>Descripción</h3>
+                <p className='font-light text-justify'>{product?.description}</p>
+            </div>
+
         </div>
     );
 }
