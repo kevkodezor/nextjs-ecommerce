832c61265f953480b5c02a52e153cd7d0c0a4a16
Kevin <keviinnalvarez2015@gmail.com>
2024-03-13 10:44:06 -0400

    new dependencie & correction auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,23 @@
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
-export const authConfig: NextAuthConfig = {
+export const { auth, signIn, signOut } = NextAuth({
     pages: {
         signIn: '/auth/login',
-        // signUp: '/register'
+        newUser: '/auth/register'
     },
-    callbacks: {},
-    providers: [],
-};
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (!parsedCredentials.success) return null;
+                const { email, password } = parsedCredentials.data;
+                return null;
+            },
+        }),
+    ],
+});
